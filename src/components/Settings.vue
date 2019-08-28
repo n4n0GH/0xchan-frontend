f4ca2043df4aada303cc5fa5ca2f396fb028b449
n4n0 <n4n0@tuta.io>
2019-08-28 14:36:10 +0200

    replace "settings" with "return" to go back if user is in settings menu

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,5 +1,20 @@
 <template>
 	<div class="w-100 text-right text-mono">
-		[<router-link :to="{name: 'settings'}">settings</router-link>]
+		<span v-if="path=='/settings'">
+			[<a href="javascript:history.back();">return</a>]
+		</span>
+		<span v-else>
+			[<router-link :to="{name: 'settings'}">settings</router-link>]
+		</span>
 	</div>
 </template>
+
+<script>
+	export default{
+		computed: {
+			path(){
+				return this.$route.path
+			}
+		}
+	}
+</script>
