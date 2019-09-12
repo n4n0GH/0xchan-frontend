2d0ec57f790818d8dcb2a84de9080832f7d08888
n4n0 <n4n0@tuta.io>
2019-09-12 23:54:57 +0200

    fixed catalog view having two "new thread" buttons

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,13 +1,9 @@
 <template>
 	<div class="row">
-		<div class="col-12 text-center">
-			<new-template v-if="openNew"/> <!-- supply slots in case of reply instead of new thread -->
-			<button class="btn btn-outline-chan" v-if="!openNew && getLogin" @click="openNew = !openNew"><i class="far fa-plus"></i> New Thread</button>
-			<hr>
-		</div>
 		<div class="col-12 mb-2">
 			<div class="row">
 				<div class="col text-mono">
+				<hr>
 					[<a href="javascript:history.back();">return</a>]
 				</div>
 			</div>
