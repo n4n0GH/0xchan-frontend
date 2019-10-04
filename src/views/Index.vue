2ad7e7b0fd570f6598b0486bf6c6134e32e3bed8
n4n0GH <n4n0@tuta.io>
2019-10-04 09:42:34 +0200

    removed disclaimer component, replaced position with instance of alert component

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -9,7 +9,17 @@
 			</div>
 		</div>
 		<div class="row justify-content-end">
-			<disclaimer />
+			<alert v-if="getDisclaimer" class="mt-2">
+				<template #content>
+					<p class="small">0xchan is a decentralized network for exchanging thoughts and ideas on any topic using Ethereum without any central organization controlling the platform.</p>
+					<p class="small">0xchan users are not anonymous by default. Most communications are encrypted via SSL, but written records are stored on the blockchain and are tied to an address that stored the content in question. Malicious parties could use this information against you; protecting your IRL identity and privacy is your own responsibility.</p>
+					<p class="small">0xchan users must adhere to the laws in their own legal jurisdiction as well as their conscience. The 0xchan developers do not condone and are not responsible for any use of the platform to conduct illegal activities.</p>
+					<p class="small">The 0xchan developers have worked hard to deliver a free platform to discuss your favorite topics with anyone in the world. But as with any software, bugs may occur. The software is provided as is, using it is at the risk of the user.</p>
+				</template>
+				<template #button>
+					<button class="mt-2 btn btn-block btn-outline-warning" @click="setBool('mutDisclaimer')"><i class="fal fa-eye-slash"></i> I understand and accept</button>
+				</template>
+			</alert>
 		</div>
 		<div class="row justify-content-end">
 			<div class="col">
@@ -23,15 +33,26 @@
 </template>
 
 <script>
-	import Disclaimer from '../components/index/Disclaimer.vue'
+	import {mapGetters, mapActions} from 'vuex'
+	import Alert from '../components/helpers/Alert.vue'
 	import NewPosts from '../components/index/NewPosts.vue'
 	import Anns from '../components/index/Anns.vue'
 
 	export default {
 		components: {
-			Disclaimer,
+			Alert,
 			NewPosts,
 			Anns
+		},
+		computed: {
+			...mapGetters([
+				'getDisclaimer'
+			])
+		},
+		methods: {
+			...mapActions([
+				'setBool'
+			])
 		}
 	}
 </script>
