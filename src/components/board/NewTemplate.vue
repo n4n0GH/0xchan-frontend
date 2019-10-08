b5e7676d08b19682e23a74c10633b25fbbfd7e88
n4n0GH <n4n0@tuta.io>
2019-10-08 12:05:30 +0200

    added nonoko option for later; users can use nonoko to not be forcefully moved to the newly created post/thread

--- src/components/board/NewTemplate.vue
+++ src/components/board/NewTemplate.vue
@@ -5,9 +5,13 @@
 				<div class="col text-left input-group">
 					<input class="border-left-0 border-top-0 form-control" type="text" placeholder="Subject (optional)" v-if="!isReply" v-model="postBody.subject">
 					<input class="border-top-0 form-control" type="text" placeholder="Name (optional)" v-model="postBody.name">
-					<div class="input-group-text ml-3 border-0 bg-chan font-chan-red">
+					<div class="input-group-text ml-3 mr-1 border-0 bg-chan font-chan-red">
 						<input type="checkbox" v-model="postBody.options" id="sage">
 						<label class="pl-2 form-check-label" for="sage">sage</label>
+					</div>
+					<div class="input-group-text ml-1 mr-3 border-0 bg-chan font-chan-red">
+						<input type="checkbox" v-model="nonoko" id="nonoko">
+						<label class="pl-2 form-check-label" for="nonoko">nonoko</label>
 					</div>
 				</div>
 			</div>
@@ -59,6 +63,7 @@
 	export default {
 		data(){
 			return{
+				nonoko: false,
 				filePreview: null,
 				fileRaw: null,
 				postContent: '',
